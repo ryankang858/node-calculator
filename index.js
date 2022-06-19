e43e5db03a6c4889e9aa5182c4cd171d2b878f4a
ryankang858 <ryankang858@gmail.com>
2022-06-19 13:20:18 -0700

    made new calculator

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-console.log(process.argv);
+// console.log(process.argv);
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[4]);
-const op = process.argv[3];
+// const num1 = parseInt(process.argv[2]);
+// const num2 = parseInt(process.argv[4]);
+// const op = process.argv[3];
 
-console.log(op);
+// console.log(op);
 
-if (op === "divide") {
-  console.log(num1 / num2);
+// if (op === "divide") {
+//   console.log(num1 / num2);
+// }
+
+switch (op) {
+    case "+":
+        console.log(num1 + num2);
+        break;
+    case "-":
+        console.log(num1 - num2);
+        break;
+    case "*":
+        console.log(num1 * num2);
+        break;
+    case "/":
+        console.log(num1 / num2);
+        break;
+    default:
+        console.log("please input a valid Number.");
+        break;
 }
+
